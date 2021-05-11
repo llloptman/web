49babd47cc9e0e7b8a8d41ed2fa73ec86c4f302d
LLlOPT <llloptman@gmail.com>
2021-05-11 17:42:50 +0300

    fix remarks

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -2,79 +2,85 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WebTest {
-    @Test
-    void ShouldPass(){
+
+    @BeforeEach
+    public void setup(){
         open("http://localhost:9999/");
-        SelenideElement form = $("#root > div > form");
+    }
+    @Test
+    void shouldPass(){
+        open("http://localhost:9999/");
+        SelenideElement form = $(".form");
         form.$("[data-test-id='name'] input").setValue("Василий");
         form.$("[data-test-id='phone'] input").setValue("+71234567890");
         form.$(".checkbox__box").click();
         form.$("button").click();
-        $("[data-test-id='order-success']").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id='order-success']").shouldHave(Condition.exactText
+                ("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
 
     }
     @Test
-    void ShouldWarnNameIsNull(){
-        open("http://localhost:9999/");
-        SelenideElement form = $("#root > div > form");
+    void shouldWarnNameIsNull(){
+        SelenideElement form = $(".form");
         form.$("button").click();
-        $(".input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+        $(".input_invalid .input__sub").shouldHave(Condition.exactText
+                ("Поле обязательно для заполнения"));
     }
     @Test
-    void ShouldWarnPhoneIsNull(){
-        open("http://localhost:9999/");
-        SelenideElement form = $("#root > div > form");
-        form.$("[data-test-id='name'] input").setValue("Васи-лий");
+    void shouldWarnPhoneIsNull(){
+        SelenideElement form = $(".form");
+        form.$("[data-test-id='name'] input").setValue("Василий");
         form.$("button").click();
-        $(".input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+        $(".input_invalid .input__sub").shouldHave(Condition.exactText
+                ("Поле обязательно для заполнения"));
     }
     @Test
-    void ShouldWarnCheckboxIsOff(){
-        open("http://localhost:9999/");
-        SelenideElement form = $("#root > div > form");
+    void shouldWarnCheckboxIsOff(){
+        SelenideElement form = $(".form");
         form.$("[data-test-id='name'] input").setValue("Василий");
         form.$("[data-test-id='phone'] input").setValue("+71234567890");
         form.$("button").click();
-        $(".input_invalid [role='presentation']").shouldHave(Condition.exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+        $(".input_invalid [role='presentation']").shouldHave(Condition.exactText
+                ("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
     }
     @Test
-    void ShouldWarnPhoneInvalidLatin(){
-        open("http://localhost:9999/");
-        SelenideElement form = $("#root > div > form");
+    void shouldWarnInvalidLatin(){
+        SelenideElement form = $(".form");
         form.$("[data-test-id='name'] input").setValue("Vasilii");//латиница
         form.$("button").click();
-        $(".input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $(".input_invalid .input__sub").shouldHave(Condition.exactText
+                ("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
     @Test
-    void ShouldWarnPhoneInvalidSymbols(){
-        open("http://localhost:9999/");
-        SelenideElement form = $("#root > div > form");
+    void shouldWarnInvalidSymbols(){
+        SelenideElement form = $(".form");
         form.$("[data-test-id='name'] input").setValue("Василий!");//восклицательный знак
         form.$("button").click();
-        $(".input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $(".input_invalid .input__sub").shouldHave(Condition.exactText
+                ("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
     @Test
-    void ShouldWarnPhoneInvalidNumbers(){
-        open("http://localhost:9999/");
-        SelenideElement form = $("#root > div > form");
-        form.$("[data-test-id='name'] input").setValue("Василий 1!");//цифры
+    void shouldWarnInvalidNumbers(){
+        SelenideElement form = $(".form");
+        form.$("[data-test-id='name'] input").setValue("Василий 1");//цифры
         form.$("button").click();
-        $(".input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $(".input_invalid .input__sub").shouldHave(Condition.exactText
+                ("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
     @Test
-    void ShouldWarnPhoneInvalid(){
-        open("http://localhost:9999/");
-        SelenideElement form = $("#root > div > form");
-        form.$("[data-test-id='name'] input").setValue("Васи-лий");
+    void shouldWarnPhoneInvalid(){
+        SelenideElement form = $(".form");
+        form.$("[data-test-id='name'] input").setValue("Василий");
         form.$("[data-test-id='phone'] input").setValue("81234567890");//без +, с 8
         form.$("button").click();
-        $(".input_invalid .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $(".input_invalid .input__sub").shouldHave(Condition.exactText
+                ("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 }
